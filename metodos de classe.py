90d33901da8c68c74306fdadf753c6303116b5c9
Alvaro <alvaro.salvino@gmail.com>
2023-08-23 21:06:43 -0300

    Update metodos de classe.py

--- metodos de classe.py
+++ metodos de classe.py
@@ -1,4 +1,4 @@
-from abc import ABC, abstractmethod
+from abc import ABC, abstractmethod, abstractproperty
 
 
 class ControleRemoto(ABC):
@@ -10,6 +10,11 @@ class ControleRemoto(ABC):
     def desligar(self):
         pass
 
+    @property
+    @abstractproperty
+    def marca(self):
+        pass
+
 class ControleTV(ControleRemoto):
     def ligar(self):
         print("Ligando TV...")
@@ -17,9 +22,33 @@ class ControleTV(ControleRemoto):
 
     def desligar(self):
         print("Desligando a TV...")
-        print("Desligado!")     
+        print("Desligado!")
+
+    @property
+    def marca(self):
+        return "LG"
+
+class ControleArCondicionado(ControleRemoto):
+    def ligar(self):   
+        print("Ligando Ar...")
+        print("Ligado")
+
+    def desligar(self):
+        print("Desligando a Ar...")
+        print("Desligado!")
+
+    @property
+    def marca(self):
+        return "philco"
 
 
 controle = ControleTV()
 controle.ligar()
 controle.desligar()
+print(controle.marca)
+
+
+controle = ControleArCondicionado()
+controle.ligar()
+controle.desligar()
+print(controle.marca)
